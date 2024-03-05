8945fc7a147ce339a4092480945142c0e80713aa
Jenn Bourke <jennfbourke@gmail.com>
2024-03-05 11:58:40 -0500

    API shell with characters.json

---
+++ db/connection.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+mongoose.set("returnOriginal", false);
+
+mongoose.connect("mongodb://localhost:27017/tedlasso_api");
+
+mongoose.connection.on('connected', () => console.log("Connected to database"));
+mongoose.connection.on('disconnected', () => console.log("Disconnected from database"));
+mongoose.connection.on('error', error => console.error("Database error", error));
+
+export default mongoose.connection
