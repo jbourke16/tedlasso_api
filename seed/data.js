83171944edafd9592d4a2aac6e014cd78d85e84e
Jenn Bourke <jennfbourke@gmail.com>
2024-03-06 10:35:56 -0500

    Data.js updated

---
+++ seed/data.js
@@ -0,0 +1,20 @@
+import db from '../db/connection.js';
+import Character from '../models/Character.js';
+import Episode from '../models/Episode.js';
+import Quote from '../models/Quote.js';
+import characterData from '../db/characters/json' assert { type: "json" };
+import episodeData from '../db/episodes.json' assert { type: "json" };
+import quoteData from '../db/quotes.json' assert { type: "json" };
+
+const insertData = async () => {
+    await db.dropDatabase();
+    await Character.create(characterData);
+    await Episode.create(episodeData);
+    await Quote.create(quoteData);
+
+    console.log("Characters, Episodes, and Quotes created.");
+
+    await db.close();
+}
+
+insertData();
