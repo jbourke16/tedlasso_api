4da8ea5a6a217760a5a8eb51d9accc9632fbef8d
Jenn Bourke <jennfbourke@gmail.com>
2024-03-06 09:33:18 -0500

    Routes added

---
+++ routes/episodes.js
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import * as controllers from '../controllers/episodes.js';
+
+const router = Router();
+
+router.get('/', controllers.getEpisodes);
+router.get('/:id', controllers.getEpisode);
+router.post('/', controllers.createEpisode);
+router.put('/:id', controllers.updateEpisode);
+router.delete('/:id', controllers.deleteEpisode);
+
+export default router;
